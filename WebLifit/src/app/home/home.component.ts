bc003e3f3bf834945fca885bd401b06466249740
marinaspr <hyejuaya@gmail.com>
2025-08-19 18:03:33 -0300

    alterações nos balões de conceito e infos

--- WebLifit/src/app/home/home.component.ts
+++ WebLifit/src/app/home/home.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const slides = document.querySelectorAll<HTMLElement>(".slide");
+      let index = 0;
+
+      const showSlide = () => {
+        slides.forEach((slide, i) => {
+          slide.classList.remove("active");
+          if (i === index) slide.classList.add("active");
+        });
+        index = (index + 1) % slides.length;
+      };
+
+      setInterval(showSlide, 4000);
+    }
+  }
 }
+
+
