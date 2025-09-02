af28c5bba074deb3d7fbad90d07ba0f594b5616e
Etec Prof. Horácio Augusto da Silveira <xareupreto@gmail.com>
2025-09-02 12:33:16 -0300

    feat: atualização

--- WebLifit/src/app/app.component.ts
+++ WebLifit/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { ConceitoComponent } from "./conceito/conceito.component";
 import { HomeComponent } from "./home/home.component";
 import { InfosComponent } from "./infos/infos.component";
 import { TelasComponent } from "./telas/telas.component";
+import { CarrosselComponent } from './carrossel/carrossel.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ConceitoComponent, HomeComponent, InfosComponent, TelasComponent],
+  imports: [RouterOutlet, ConceitoComponent, HomeComponent, InfosComponent, TelasComponent, CarrosselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ WebLifit/src/app/carrossel/carrossel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-carrossel',
+  imports: [NgbCarouselModule],
+  templateUrl: './carrossel.component.html',
+  styleUrl: './carrossel.component.css'
+})
+export class CarrosselComponent {
+
+}
